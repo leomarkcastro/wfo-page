f129173bd960b3cf969b1734bfb8242e5313b84c
Leo <leomarkcastro123@gmail.com>
2025-01-30 22:19:37 +0800

    feat: redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { useRouter } from 'next/navigation';
+
 export default function DashboardPage() {
+  const router = useRouter();
+
+  router.replace('/admin/dashboard');
+
   return <p>Hello</p>;
 }
