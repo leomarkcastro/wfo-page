059a21b029a260e887e7fc3f3b656784a9669e50
Leo <leomarkcastro123@gmail.com>
2025-02-13 18:53:32 +0800

    fix: api-sync

--- graphql/declarations/admin.ts
+++ graphql/declarations/admin.ts
@@ -1,13 +1,5 @@
 import { graphql } from '../generated';
 
-export const Api_sync_users = graphql(`
-  mutation Api_sync_users($input: Api_sync_usersInput!) {
-    api_sync_users(input: $input) {
-      count
-    }
-  }
-`);
-
 export const Api_sync_crm_wordpress = graphql(`
   mutation Api_sync_crm_wordpress($input: Api_sync_crm_wordpressInput!) {
     api_sync_crm_wordpress(input: $input) {
@@ -20,6 +12,30 @@ export const Api_sync_crm_netforum = graphql(`
   mutation Api_sync_crm_netforum($input: Api_sync_crm_netforumInput!) {
     api_sync_crm_netforum(input: $input) {
       success
+    }
+  }
+`);
+
+export const Api_sync_from_netforum = graphql(`
+  mutation Api_sync_from_netforum($input: Api_sync_from_netforumInput!) {
+    api_sync_from_netforum(input: $input) {
+      count
+    }
+  }
+`);
+
+export const Api_sync_from_wordpress = graphql(`
+  mutation Api_sync_from_wordpress($input: Api_sync_from_wordpressInput!) {
+    api_sync_from_wordpress(input: $input) {
+      count
+    }
+  }
+`);
+
+export const Api_timecheck = graphql(`
+  query Api_timecheck($input: Api_timecheckInput!) {
+    api_timecheck(input: $input) {
+      time
     }
   }
 `);
